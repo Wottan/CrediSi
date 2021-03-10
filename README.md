37a8a48f486988894e04aa97eac4d4f7f4cd17b8
Nicolas Pirasoft <nferrazzuolo@pirasoft.com.ar>
2021-03-10 12:58:38 -0300

    initial docker setup

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rrhh
 
+copy .env.default to .env
+
+to build execute
+term> docker-compose build app
+
+to run execute
+term> docker-compose up
+
+to install dependencies
+term> docker-compose exec app composer install
+
+to encrypt sessions
+term> docker-compose exec app php artisan key:generate
+
+to see logs of a service
+term> docker-compose logs nginx
+
+to shutdown the app
+term>docker-compose down
+
+
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view("/home", "home")
+    ->middleware("auth")
+    ->name("home");
+
+Route::view("/login", "login")
+    ->name("login");
+
+Route::post(
+    "/login",
+    [App\Http\Controllers\LoginController::class, "authenticate"]
+)->name("login-post");
+
+Route::post(
+    "/logout",
+    [App\Http\Controllers\LoginController::class, "logout"]
+)->name("logout");
