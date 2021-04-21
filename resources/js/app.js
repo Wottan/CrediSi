224b188bd585fdb8536ed10e76cd16f80e6c6910
ivanpirasoft <isuchan@pirasoft.com.ar>
2021-04-21 12:03:46 -0300

    Vue integration component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import Vue from 'vue'
+
+//Main pages
+import router from './router';
+import App from './App';
+
+
+const app = new Vue({
+    router,
+    el: '#app',
+    render: h => h(App)
+});
+
