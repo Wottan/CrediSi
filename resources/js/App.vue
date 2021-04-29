2486a91b49ac594a2485fc946dc5b082b4ccd900
ivanpirasoft <isuchan@pirasoft.com.ar>
2021-04-29 12:07:14 -0300

    Vuetify Integration in Login page

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view></router-view>
+  <v-app app>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 <script>
 export default {};
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,16 @@ import router from './router';
 import App from './App';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import vuetify from './plugins/vuetify';
+import Vuelidate from 'vuelidate';
 
+
+Vue.use(Vuelidate);
 Vue.use(VueAxios, axios);
 
 const app = new Vue({
     router,
+    vuetify,
     el: '#app',
     render: h => h(App)
 });
