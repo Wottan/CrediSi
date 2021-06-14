4993595be612b40d3ce6e25e4ae003fc04f1d2f1
Damian Pirasoft <dkober@pirasoft.com.ar>
2021-06-14 20:51:20 +0000

    Shift occurrences

--- app/Services/ShiftService.php
+++ app/Services/ShiftService.php
@@ -2,43 +2,94 @@
 
 namespace App\Services;
 
+use App\Exceptions\ServiceException;
 use App\Models\Shift;
+use App\Models\ShiftEvent;
+use Carbon\Carbon;
+use DateTime;
 
-class ShiftService {
+class ShiftService
+{
 
     /**
      * All events, with their occurences applied
+     * 
+     * @return Shift[]
      */
-    public function all(): array  {
-        return Shift::with(['events','user'])->get()->toArray();
+    public function all(): iterable
+    {
+        $shifts = Shift::with(['events', 'user'])->get();
+        $shifts->each(function (Shift $shift) {
+            self::recalculate($shift);
+        });
+        return $shifts;
     }
 
     /**
      * Creates a new shift, handles the relationships
      */
-    public function create(array $array): Shift {
+    public function create(array $array): Shift
+    {
         $shift = Shift::create($array);
         $shift->events()->createMany($array["events"] ?: []);
-        return Shift::with(['events','user'])->findOrFail($shift->id);
+        return Shift::with(['events', 'user'])->findOrFail($shift->id);
     }
 
     /**
      * Updates a shift and related events
      */
-    public function update(string $id, array $array): Shift {
+    public function update(string $id, array $array): Shift
+    {
         $shift = Shift::findOrFail($id);
         $shift->update($array);
         $shift->events()->delete();
         $shift->events()->createMany($array["events"] ?: []);
-        return Shift::with(['events','user'])->findOrFail($shift->id);
+        return Shift::with(['events', 'user'])->findOrFail($shift->id);
     }
 
     /**
      * Deletes a shift and related events
      */
-    public function delete(string $id) {
+    public function delete(string $id)
+    {
         $shift = Shift::findOrFail($id);
         $shift->events()->delete();
         $shift->delete();
+    }
+
+    /**
+     * Recalculate events recurrence
+     */
+    private static function recalculate(Shift $shift): Shift
+    {
+        foreach ($shift->events() as $event) {
+            self::recalculateEvent($event);
+        }
+        return $shift;
+    }
+
+    /**
+     * Recalculate event's recurrence
+     */
+    private static function recalculateEvent(ShiftEvent $event): ShiftEvent
+    {
+        $event->start = self::calculateNextOccurrence($event->start, $event->recurrence);
+        $event->end = self::calculateNextOccurrence($event->end, $event->recurrence);
+        return $event;
+    }
+
+    /**
+     * Calculate next occurrence
+     */
+    private static function calculateNextOccurrence(DateTime $moment, string $recurrenceType): DateTime
+    {
+        $now = Carbon::now();
+        if ("none" === $recurrenceType) {
+            return $moment;
+        } else if ("weekly" === $recurrenceType) {
+            return Carbon::instance($moment)->addWeeks($now->diffInWeeks($moment))->toDateTime();
+        } else {
+            throw new ServiceException("Unrecognized recurrence type: $recurrenceType");
+        }
     }
 }
