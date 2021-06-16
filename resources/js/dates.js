d133a527c2dd147557cf158709341f534cb18ba0
Damian Pirasoft <dkober@pirasoft.com.ar>
2021-06-16 14:37:33 +0000

    better calendar

--- resources/js/dates.js
+++ resources/js/dates.js
@@ -1,4 +1,4 @@
-import { DateTime, Settings } from "luxon";
+import { DateTime, Settings, Duration } from "luxon";
 
 Settings.defaultZoneName = "utc";
 
@@ -8,17 +8,87 @@ const DateConstants = {
     DATETIME_FORMAT: "yyyy-MM-dd HH:mm"
 }
 
+function currentTimeStr() {
+    return DateTime.now().toFormat(DateConstants.TIME_FORMAT, { zone: "utc" });
+}
+
+function currentDateStr() {
+    return DateTime.now().toFormat(DateConstants.DATE_FORMAT, { zone: "utc" });
+}
+
+function currentDateTimeMillis() {
+    return DateTime.now().toMillis();
+}
+
+function currentDateTimeStr() {
+    return DateTime.now().toFormat(DateConstants.DATETIME_FORMAT, { zone: "utc" });
+}
+
+function fromISOToDateTimeStr(iso) {
+    if (!iso) {
+        return null;
+    }
+    return DateTime.fromISO(iso, { zone: "utc" }).toFormat(DateConstants.DATETIME_FORMAT, { zone: "utc" });
+}
+
+function fromISOToDateStr(iso) {
+    if (!iso) {
+        return null;
+    }
+    return DateTime.fromISO(iso, { zone: "utc" }).toFormat(DateConstants.DATE_FORMAT, { zone: "utc" });
+}
+
+function fromISOToMillis(iso) {
+    if (!iso) {
+        return null;
+    }
+    return DateTime.fromISO(iso, { zone: "utc" }).toMillis();
+}
+
+function fromMillisToDateTimeStr(millis) {
+    if (!millis) {
+        return null;
+    }
+    return DateTime.fromMillis(millis).setZone("utc").toFormat(DateConstants.DATETIME_FORMAT, { zone: "utc" });
+}
+
+function fromMillisToTimeStr(millis) {
+    if (!millis) {
+        return null;
+    }
+    return DateTime.fromMillis(millis).setZone("utc").toFormat(DateConstants.TIME_FORMAT, { zone: "utc" });
+}
+
+
+function fromISOTimeToMillis(iso) {
+    if (!iso) {
+        return null;
+    }
+    return Duration.fromISO(iso, { zone: "utc" }).toMillis();
+}
+
+function hourDifferenceFromMillis(start, end) {
+    if (!start || !end) {
+        return null;
+    }
+    return Duration.fromMillis(end).toObject().hours - Duration.fromMillis(start).toObject().hours;
+}
+
 const DateFunctions = {
-    currentTimeStr: () => DateTime.now().toFormat(DateConstants.TIME_FORMAT, {  zone: "utc"}),
-    currentDateStr: () => DateTime.now().toFormat(DateConstants.DATE_FORMAT, {  zone: "utc"}),
-    currentDateTimeMillis: () => DateTime.now().toMillis(),
-    currentDateTimeStr: () => DateTime.now().toFormat(DateConstants.DATETIME_FORMAT, {  zone: "utc"}),
-    fromISOToDateTimeStr: (iso) => DateTime.fromISO(iso, {  zone: "utc"}).toFormat(DateConstants.DATETIME_FORMAT, {  zone: "utc"}),
-    fromISOToDateStr: (iso) => DateTime.fromISO(iso, {  zone: "utc"}).toFormat(DateConstants.DATE_FORMAT, {  zone: "utc"}),
-    fromISOToMillis: (iso) => DateTime.fromISO(iso, {  zone: "utc"}).toMillis(),
-    fromMillisToDateTimeStr: (millis) => DateTime.fromMillis(millis).setZone("utc").toFormat(DateConstants.DATETIME_FORMAT, {  zone: "utc"}),
+    currentTimeStr,
+    currentDateStr,
+    currentDateTimeMillis,
+    currentDateTimeStr,
+    fromISOToDateTimeStr,
+    fromISOToDateStr,
+    fromISOToMillis,
+    fromISOTimeToMillis,
+    fromMillisToDateTimeStr,
+    fromMillisToTimeStr,
+    hourDifferenceFromMillis,
 }
 
 export {
+    DateConstants,
     DateFunctions
 };
